7563af82975f1866f2a0c4b74d021ca676282680
Vladislaw Dzhabiev <vljkee@gmail.com>
2022-07-28 10:58:48 +0300

    Добавьте файлы проекта.

---
+++ BusPath/FileReader/Models/BusRoute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusPath.Models
+{
+    public class BusRoute
+    {
+        public BusRoute(int busId, /*int stopQty,*/ DateTime departureTime, int price, List<BusStop> busStops)
+        {
+            BusId = busId;
+            //StopQty = stopQty;
+            DepartureTime = departureTime;
+            Price = price;
+            BusStops = busStops;
+        }
+
+        int BusId { get; }
+        //int StopQty { get; }
+        DateTime DepartureTime { get; }
+        int Price { get; }
+        List<BusStop> BusStops { get; }
+    }
+}
